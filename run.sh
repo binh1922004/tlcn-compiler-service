42184f50ad49c1982c1d663a32f01d313882c639
binh1922004 <binh1920042018@gmail.com>
2025-12-30 14:10:07 +0700

    new version

--- run.sh
+++ run.sh
@@ -8,4 +8,4 @@ OJ_IMAGE=oj:4.0
 docker build -t $OJ_IMAGE compiler/
 echo "Run the compiler service container..."
 COMPILER_SERVICE_IMAGE=bnoj-compiler:4.0
-sudo docker run -d --name compiler-service -v /var/run/docker.sock:/var/run/docker.sock -v submission_data:/app/oj -v problem_data:/app/problems COMPILER_SERVICE_IMAGE
+sudo docker run -d --name compiler-service -v /var/run/docker.sock:/var/run/docker.sock -v submission_data:/app/oj -v problem_data:/app/problems $COMPILER_SERVICE_IMAGE
